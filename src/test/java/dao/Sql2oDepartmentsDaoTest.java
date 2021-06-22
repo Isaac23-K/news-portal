c15dff693aaff0b6e6b5c52e4f196fdf7794a788
Isaac23-K <ikaptoge@yahoo.com>
2021-06-22 14:19:11 +0300

    modified Sql2oDepartment.java test

--- src/test/java/dao/Sql2oDepartmentsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentsDaoTest.java
@@ -1,17 +1,42 @@
 package dao;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-import static org.junit.Assert.*;
+class Sql2oDepartmentsDaoTest {
+    private static Sql2oDepartmentsDao sql2oDepartmentsDao;
+    private static Sql2oNewsDao sql2oNewsDao;
+    private static Sql2oUserDao sql2oUserDao;
+    private static Connection conn;
 
-public class Sql2oDepartmentsDaoTest {
-
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/company_news_test";
+        Sql2o sql2o = new Sql2o(connectionString, "isaac", "kaptoge");
+        sql2oDepartmentsDao = new Sql2oDepartmentsDao(sql2o);
+        sql2oNewsDao = new Sql2oNewsDao(sql2o);
+        sql2oUserDao = new Sql2oUserDao(sql2o);
+        System.out.println("connected to database");
+        conn = sql2o.open();
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterEach
+    void tearDown() throws  Exception{
+        sql2oDepartmentsDao.clearAll();
+        sql2oNewsDao.clearAll();
+        sql2oUserDao.clearAll();
+    }
+    @AfterAll
+    public static void shutDown() throws Exception{
+        conn.close();
+        System.out.println("connection is closed");
+    }
+
+    @Test
+    void adding_() {
     }
 }
