3f19b48c947bf709d769f462b5f3be377a04f12f
Isaac23-K <ikaptoge@yahoo.com>
2021-06-22 14:03:26 +0300

    addded code for department dao

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,13 @@
 package dao;
 
+import models.Department;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    void add(Department department);
+
+    List<Department> getAllDepartment();
+
+    void clearAll();
 }
