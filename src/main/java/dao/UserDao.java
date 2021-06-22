14c50534a614a11c583fc6c298f053dc13f1601e
Isaac23-K <ikaptoge@yahoo.com>
2021-06-22 14:29:44 +0300

    added userdao interface code

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    //create
+    void add(User user);
+
+    //read
+    List<User> getAll();
+    User getUserById(int id);
+
+    //delete
+    // void deleteById(int id);
+    void clearAll();
 }
